4969446c3ffc15ec0482e6d9dc538729c0afaead
AlexisMendozaS <armandomendoza988@gmail.com>
2024-04-18 22:51:12 -0600

    inicializacion de las variables del proyecto

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Desafio {
+public class Desafio {
+    public static void main(String[] args) {
+        String nombre = "Tony Stark";
+        String tipoDeCuenta = "Corriente";
+        double saldo = 1599.99;
+
+        System.out.println("***********************");
+        System.out.println("\nNombre del Cliente: " + nombre);
+        System.out.println("Tipo de Cuenta: " + tipoDeCuenta);
+        System.out.println("Su saldo disponible es: " + saldo + "$");
+    }
 }
